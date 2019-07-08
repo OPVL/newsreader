231b222cc3b52ae35d3130b608e72f96db519407
Lloyd <lloydculpepper4@gmail.com>
2019-07-08 08:37:40 +0100

    source update

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-import { ftruncateSync } from "fs";
-
-const apiKey = '';
+const apiKey = 'f8df029c38894949b77be86616a3bc58';
 const main = document.querySelector('main');
 const sourceSelector = document.querySelector('#sourceSelector');
-
 window.addEventListener('load', e => {
     updateNews();
+    updateSources();
 });
 
+async function updateSources() {
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+    const json = res.json();
+
+    sourceSelector.innerHTML = json.sources
+        .map(src => `<option value="${src.id}">${src.name}</option>`)
+        .join('\n');
+}
+
 async function updateNews() {
-    const res = await fetch(`${apiKey}`);
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`);
     const json = await res.json();
 
     main.innerHTML = json.articles
@@ -22,8 +29,8 @@ function createArticle(article) {
     <div class="article">
         <a href="${article.url}">
             <h2>${article.title}</h2>
-            <img src="${article.urlToImage}" alt="${article.title}">
-            <p>${article.desctiption}</p>
+            <img src="${article.urlToImage}">
+            <p>${article.description}</p>
         </a>
     </div>`;
 }
